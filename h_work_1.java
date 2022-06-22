eca4a15f063d3f9d1dee99388ab8e2437abae88d
mkrqazg2 <9366086@inbox.ru>
2022-06-22 13:16:45 +0300

    исправлены замечания

--- h_work_1.java
+++ h_work_1.java
@@ -1,19 +1,22 @@
 // Написать программу вычисления n-ого треугольного числа.
 
+import java.util.Scanner;
 
 public class h_work_1 {
 
     public static void main(String[] args) {
-        int n = 7;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int n = in.nextInt();
         for (int i = 1; i <= n; i++) {
             int triangular = 0;
 
             for (int j = 1; j <= i; j++) {
                 triangular = triangular + j;
             }
-            System.out.println(i + " = " + triangular);
-       }
         
-    }
+        System.out.println(i + " = " + triangular);
+        
+        }
+    } 
 }
-
